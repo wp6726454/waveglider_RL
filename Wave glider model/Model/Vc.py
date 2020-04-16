a7693ba461e267bba57264b309b5c890fe7726d7
767312562 <45227093+767312562@users.noreply.github.com>
2020-04-16 16:42:28 +0800

    Update Vc.py

--- Wave glider model/Model/Vc.py
+++ Wave glider model/Model/Vc.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 
-def Vc(dir_, speed, eta):
-    phi = eta.item(3)
+# vector of current velocity
+def Vc(dir_, speed, eta):  # current direction, current speed, vessel's position and attitude vector
+    phi = eta.item(3)  # vessel's heading of vessel
     vc_x = speed * np.cos(dir_-phi)
     vc_y = speed * np.sin(dir_-phi)
     return np.array([[vc_x], [vc_y], [0], [0]], float)
