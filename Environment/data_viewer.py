dd92c6f74f3eedd2ff9b5bea7c67c6798434400d
767312562 <45227093+767312562@users.noreply.github.com>
2020-04-23 14:02:52 +0800

    Update data_viewer.py

--- Environment/data_viewer.py
+++ Environment/data_viewer.py
@@ -2,53 +2,54 @@ import matplotlib.pyplot as plt
 
 
 # data monitor during simulation
-def data_viewer(x1, y1, phit, t,
-                u1='', T='', rudder_angle='',
-                Ffoil_x=''):
-    plt.figure(1)
+def data_viewer(x1, y1, u1, phit, rudder_angle, t,
+                xlim_left=0, xlim_right=200,ylim_left=-100, ylim_right=100,
+                goal_x=50, goal_y=50,
+                T='', Ffoil_x=''):
 
-    path = plt.subplot(3, 2, 1)
-    path.plot(y1, x1, '-r')
+    plt.figure(1, figsize=(14, 7))
+    grid = plt.GridSpec(3, 2, wspace=0.5, hspace=0.5)
+    path = plt.subplot(grid[0:3, 0])
+    path.plot(y1, x1, label='Sailing path', color='b')
     path.set_title('Path')
+    path.axis([ylim_left, ylim_right, xlim_left, xlim_right])
+    path.scatter(goal_y, goal_x, label='Goal', color='r')
     path.set_ylabel('x(m)')
     path.set_xlabel('y(m)')
+    path.legend()
 
-    heading = plt.subplot(3, 2, 2)
-    heading.plot(t, phit, '-b', label='phit')
+    heading = plt.subplot(grid[0, 1])
+    heading.plot(t, phit, '-r', label='phit')
     heading.set_title('Heading')
     heading.set_ylabel('Course(rad)')
-    heading.set_xlabel('Time(s)')
+
+    speed_plot = plt.subplot(grid[1, 1])
+    speed_plot.plot(t, u1, '-r')
+    speed_plot.set_title('Sailing speed')
+    speed_plot.set_ylabel('Speed(m/s)')
+
+    rudder_angle_plot = plt.subplot(grid[2, 1])
+    rudder_angle_plot.plot(t, rudder_angle, '-r')
+    rudder_angle_plot.set_title('Rudder angle')
+    rudder_angle_plot.set_ylabel('Rudder angle(deg)')
+    rudder_angle_plot.set_xlabel('Time(s)')
 
     if T:
-        T_plot = plt.subplot(3, 2, 3)
+        plt.figure(2, figsize=(12, 4))
+        T_plot = plt.subplot(1, 2, 1)
         T_plot.plot(t, T, '-r')
         T_plot.set_title('Tether force')
         T_plot.set_ylabel('T(N)')
         T_plot.set_xlabel('Time(s)')
 
-    if u1:
-        speed_plot = plt.subplot(3, 2, 4)
-        speed_plot.plot(t, u1, '-r')
-        speed_plot.set_title('Sailing speed')
-        speed_plot.set_ylabel('Speed(m/s)')
-        speed_plot.set_xlabel('Time(s)')
-
-    if rudder_angle:
-        rudder_angle_plot = plt.subplot(3, 2, 5)
-        rudder_angle_plot.plot(t, rudder_angle, '-r')
-        rudder_angle_plot.set_title('Rudder angle')
-        rudder_angle_plot.set_ylabel('Rudder angle(deg)')
-        rudder_angle_plot.set_xlabel('Time(s)')
-
     if Ffoil_x:
-        Ffoil_x_plot = plt.subplot(3, 2, 6)
+        plt.figure(2, figsize=(12, 4))
+        Ffoil_x_plot = plt.subplot(1, 2, 2)
         Ffoil_x_plot.plot(t, Ffoil_x, '-r')
         Ffoil_x_plot.set_title('Foil force')
         Ffoil_x_plot.set_ylabel('Foil force(N)')
         Ffoil_x_plot.set_xlabel('Time(s)')
 
-
-    plt.tight_layout()
-    plt.pause(0.00000000000000001)
+    plt.pause(0.00001)
 
 
